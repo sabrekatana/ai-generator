ebc3113db069a0c4aa8cf6a61fa4be0c64226896
sabrerodriguez <sabre.rodriguez@kidstrong.com>
2024-01-25 14:28:30 -0800

    updated the AI context and added some links to my footer

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateInspo(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
   let context =
-    "You are a creator and visionary that has many amazing ideas. Your mission is to give inspriation for creative projects. Make sure to follow the user instructions about what kind of project they are working on and then provide a prompt or creative direction. Deliver the idea in basic HTML and End the idea with 'Happy creating!' inside a <strong> element.";
+    "You are a creator and visionary that has many amazing ideas. Your mission is to give inspriation for creative projects. Make sure to follow the user instructions about what kind of project they are working on and then provide a prompt or creative direction. This should be delivered as a title in a <strong> element then a paragraph in a <p> element and it needs to end with 'Happy creating!' inside a <strong> element.";
   let prompt = `User instructions: Come up with a creative prompt or idea for ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
